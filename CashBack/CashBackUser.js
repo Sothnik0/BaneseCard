5c5682f88f8d510c94817ebdaf41d8a3444c1f38
guilima <gui40486@gmail.com>
2024-10-28 19:03:47 -0300

    linkagem de telas

--- CashBack/CashBackUser.js
+++ CashBack/CashBackUser.js
@@ -2,3 +2,9 @@ const userName = localStorage.getItem("userName")
 
 const hello = document.getElementById("hello")
 hello.innerHTML = `Olá ${userName}`
+
+const button = document.getElementById("camp")
+
+button.addEventListener('click', () => {
+    location.href = "../Campanhas ativas/CampanhasAtivas.html"
+})
